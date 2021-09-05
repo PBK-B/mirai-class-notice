bee2504a7a6c2504567af11ac1ea239db6d2a77c
PBK-B <pbk-b@PBK6.cn>
2021-09-05 15:03:16 +0800

    fix: repair multiple redirection problems when landing

--- controllers/admin.go
+++ controllers/admin.go
@@ -13,22 +13,38 @@ type AdminController struct {
 func (c *AdminController) Get() {
 	_, _, is := isLogin(c)
 
+	flash := beego.NewFlash()
+	if _, ok := flash.Data["notice"]; ok {
+		c.TplName = "admin.tpl"
+		return
+	}
+
 	if !is {
 		// token 获取失败或失效，或用户被禁用将跳转登陆
 		c.Redirect("/login", 301)
 		c.Finish()
+		return
 	}
 
 	c.TplName = "admin.tpl"
 }
 
 func (c *AdminController) LoginPage() {
-	_, _, is := isLogin(c)
+	token, _, is := isLogin(c)
+
 	if is {
+
+		flash := beego.NewFlash()
+		flash.Notice(token)
+		flash.Store(&c.Controller)
+
 		c.Redirect("/admin", 301)
-	} else {
-		c.TplName = "login.tpl"
+		c.Finish()
+		return
 	}
+
+	c.TplName = "login.tpl"
+
 }
 
 func isLogin(c *AdminController) (token string, user models.Users, isLogin bool) {
